bf6199dd9513548607e3c34e67757dbe2b53bf06
DontTreadMe <31659942+DontTreadMe@users.noreply.github.com>
2018-08-25 07:04:35 +0300

    Update react.js

--- react.js
+++ react.js
@@ -2,41 +2,46 @@ class Controls extends React.Component {
   render() {
     return (
       <div id="controlsClock">
-        <button id="start_stop" onClick={this.props.startStop}>
-          <i className="fa fa-play" />
-          <i className="fa fa-play fa fa-pause" />
+        <button id="start_stop" className="btn"  
+          onClick={this.props.startStop}>
+          <i className="fa fa-play fa-2x" />
+          <i className="fa fa-play fa fa-pause fa-2x" />
         </button>
-        <button id="reset" className="fa fa-refresh" onClick={this.props.refresh} />
+        <button id="reset" className="btn" onClick={this.props.refresh}>
+          <i className="fa fa-refresh fa-2x" />
+        </button>
       </div>
     );
   }
 }
 const Display = (props) => {
-  const min = Math.floor(props.timeLeft /60) < 10 ? '0' + Math.floor(props.timeLeft /60) : 
+  const min = Math.floor(props.timeLeft /60) < 10 ? 
+  '0' + Math.floor(props.timeLeft /60) : 
   Math.floor(props.timeLeft /60);
-  const sec = (props.timeLeft - min * 60) < 10 ? '0' + (props.timeLeft - min * 60) : 
+  const sec = (props.timeLeft - min * 60) < 10 ? 
+  '0' + (props.timeLeft - min * 60) : 
   props.timeLeft - min * 60;
   return(
-    <div>
+    <div id="display" style={props.alarmColor}>
       <div id="timer-label">{props.mode}</div>
-      <div id="time-left">{`${min} : ${sec}`}</div>
+      <div id="time-left">{`${min}:${sec}`}</div>
     </div>
   );
 }
 const Lengths = (props) => {
   return (
-    <div>
+    <div className="sessionBreak">
       <div id={props.idLabel}>{props.title}</div>
-      <div>
-        <button id={props.idDubleDecrement} className="btn fa fa-angle-double-down" 
-        onClick={props.setLength} value={-10} />
-        <button id={props.idDecrement} className="btn fa fa-angle-down" 
-        onClick={props.setLength} value={-1} />
-        <span id={props.idLength}>{props.length}</span>
-        <button id={props.idIncrement} className="btn fa fa-angle-up" 
-        onClick={props.setLength} value={1} />
-        <button id={props.idDubleIncrement} className="btn fa fa-angle-double-up" 
-        onClick={props.setLength} value={10} />
+      <div>    
+        <div id={props.idLength}>{props.length}</div>
+        <button id={props.idIncrement} className="btn" 
+          onClick={props.setLength} value={1}>
+          <i className="fa fa-angle-up fa-3x" />
+        </button>
+        <button id={props.idDecrement} className="btn" 
+          onClick={props.setLength} value={-1}>
+          <i className="fa fa-angle-down fa-3x" />
+        </button>    
       </div>
     </div>
   )
@@ -50,7 +55,8 @@ class Pomodoro extends React.Component {
       mode: 'Session',
       timeLeft: 1500,
       intervalID: '',
-      pause: 1500
+      paused: 1500,
+      alarmColor: {color: 'white'}
     }
     this.setBreak = this.setBreak.bind(this);
     this.setSession = this.setSession.bind(this);
@@ -59,6 +65,7 @@ class Pomodoro extends React.Component {
     this.decrementTimer = this.decrementTimer.bind(this);
     this.toggleMode = this.toggleMode.bind(this);
     this.startCountdown = this.startCountdown.bind(this);
+    this.warning = this.warning.bind(this);
   }
   setBreak(e) {
     if (!this.state.intervalID) {
@@ -76,7 +83,7 @@ class Pomodoro extends React.Component {
       this.setState({
         sessionLength: currentValue,
         timeLeft: currentValue * 60,
-        pause: currentValue * 60
+        paused: currentValue * 60
       });
     }
   }
@@ -88,7 +95,8 @@ class Pomodoro extends React.Component {
       mode: 'Session',
       timeLeft: 1500,
       intervalID: '',
-      pause: 1500
+      paused: 1500,
+      alarmColor: {color: 'white'}
     });
     const myAudio = document.getElementById("beep");
     myAudio.pause();
@@ -98,9 +106,9 @@ class Pomodoro extends React.Component {
     this.state.intervalID ? 
     (
       clearInterval(this.state.intervalID),
-      this.setState({intervalID: '', pause: this.state.timeLeft})
+      this.setState({intervalID: '', paused: this.state.timeLeft})
     ) : (
-      this.setState({timeLeft: this.state.pause}),
+      this.setState({timeLeft: this.state.paused}),
       this.startCountdown()
     );
   }
@@ -118,46 +126,57 @@ class Pomodoro extends React.Component {
   toggleMode() {
     let timer = this.state.timeLeft;
     this.buzzer(timer);
-    if (this.state.timeLeft < 0) {
+    this.warning(timer);
+    if (timer < 0) {
       this.state.mode === 'Session' ? 
       this.setState({
         mode: 'Break',
-        timeLeft: this.state.breakLength *60
+        timeLeft: this.state.breakLength *60,
+        alarmColor: {color: 'white'}
       }) : 
       this.setState({
         mode: 'Session',
-        timeLeft: this.state.sessionLength * 60
+        timeLeft: this.state.sessionLength * 60,
+        alarmColor: {color: 'white'}
       })
     }
   }
-  buzzer(timer) {
-    console.log(timer);
+  buzzer(timer) {    
     if (timer === 0) {
-      console.log('beep');
       const myAudio = document.getElementById("beep");
       myAudio.play()
     }
   }
+  warning(timer) {
+    let warn = timer < 61 ? 
+      this.setState({alarmColor: {color: 'tomato'}}) : 
+      this.setState({alarmColor: {color: 'white'}});
+  }
   render() {
     return (
-      <div>
-        Pomodoro Clock
-        <div>
-          <Lengths idLabel="break-label" title="Break Length" idDubleDecrement="break-duble-decrement" 
-          setLength={this.setBreak} idDecrement="break-decrement" 
-          idLength="break-length" length={this.state.breakLength} 
-          idIncrement="break-increment" idDubleIncrement="break-duble-increment" />
-          <Lengths idLabel="session-label" title="Session Length" idDubleDecrement="session-duble-decrement" 
-          setLength={this.setSession} idDecrement="session-decrement" 
-          idLength="session-length" length={this.state.sessionLength} 
-          idIncrement="session-increment" idDubleIncrement="session-duble-increment" />
-        </div>
-        <div>
-          <Display mode={this.state.mode} timeLeft={this.state.timeLeft} />
-          <Controls refresh={this.refresh} startStop={this.startStop} />
+      <div id="conteiner">        
+        <div id="clock">
+          <div id="clock-title">Pomodoro Clock</div>
+          <div>
+            <Display mode={this.state.mode} timeLeft={this.state.timeLeft} 
+              alarmColor={this.state.alarmColor} />
+            <Controls refresh={this.refresh} startStop={this.startStop} />
+          </div>
+          <div id="lengths">
+            <Lengths idLabel="session-label" title="Session Length" 
+              setLength={this.setSession} idDecrement="session-decrement" 
+              idLength="session-length" length={this.state.sessionLength} 
+              idIncrement="session-increment" />
+            <Lengths idLabel="break-label" title="Break Length" 
+              setLength={this.setBreak} idDecrement="break-decrement" 
+              idLength="break-length" length={this.state.breakLength} 
+              idIncrement="break-increment" />
+          </div>
         </div>
         <audio id="beep" preload="auto">
-          <source src="http://static1.grsites.com/archive/sounds/bells/bells003.mp3" type="audio/mpeg" />
+          <source 
+            src="https://d1490khl9dq1ow.cloudfront.net/sfx/mp3preview/jg-032316-sfx-video-game-beeps-4.mp3" 
+          type="audio/mpeg" />
         </audio>
       </div>
     );
